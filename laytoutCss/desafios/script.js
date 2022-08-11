300a7bcb140f5a36558efb34337cc22645e3fc52
thalesmacri <thalesmacri5@gmail.com>
2022-08-11 07:54:40 -0300

    first commit

---
+++ laytoutCss/desafios/script.js
@@ -0,0 +1,41 @@
+const openSpy = document.getElementById('spy')
+
+openSpy.onclick = function(){
+    open('https://betteranime.net/anime/legendado/spy-x-family/episodio-12');
+}
+
+const openOver = document.getElementById('over')
+
+openOver.onclick = function(){
+    open('https://betteranime.net/anime/legendado/overlord-iv/episodio-05');
+}
+
+const openTitan = document.getElementById('titan')
+
+openTitan.onclick = function(){
+    open('https://betteranime.net/anime/legendado/shingeki-no-kyojin-the-final-season-part-2/episodio-12');
+}
+
+const openOp = document.getElementById('op')
+
+openOp.onclick = function(){
+    open('https://betteranime.net/anime/legendado/one-piece/episodio-1027');
+}
+
+const openOne = document.getElementById('one')
+const openDs = document.getElementById('ds')
+const openFt = document.getElementById('ft')
+const openBc = document.getElementById('bc')
+
+openOne.onclick = function(){
+    open('https://betteranime.net/anime/legendado/one-punch-man-2');
+}
+openDs.onclick = function(){
+    open('https://betteranime.net/anime/legendado/kimetsu-no-yaiba-yuukaku-hen');
+}
+openFt.onclick = function(){
+    open('https://betteranime.net/anime/legendado/fairy-tail');
+}
+openBc.onclick = function(){
+    open('https://betteranime.net/anime/legendado/black-clover');
+}
